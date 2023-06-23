443f878176ba965576a5993321cdd9f5e1290066
Brandon Bocanegra <brandon@veridise.com>
2023-06-23 10:46:00 -0700

    Swizzle NavbarItem for conditional rendering of docsVersionDropdown
    These changes are necessary so that we can declare a
    docsVersionDropdown per documentation instance and only display a
    single version dropdown on the site navbar.

---
+++ src/theme/NavbarItem/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useLocation } from '@docusaurus/router';
+import NavBarItem from '@theme-original/NavbarItem';
+
+export default function CustomNavbarItem(props) {
+  const { docsPluginId, type } = props;
+  const { pathname } = useLocation();
+
+  // Skip rendering version dropdown if the declared component does not match
+  // the current route
+  if (type === "docsVersionDropdown" &&
+      pathname.search(new RegExp(`^/${docsPluginId}/`, 'g')) === -1)
+  {
+    return null;
+  }
+
+  return (
+    <>
+      <NavBarItem {...props} />
+    </>
+  );
+}
