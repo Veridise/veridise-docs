36a9c12535ac5ac1e6e3bc4de403cf52ad8d3ed8
Brandon Bocanegra <brandon@veridise.com>
2024-06-25 14:21:06 -0700

    [SAS-648] fix formatting
    These changes were produced by running "npm run format".

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,43 +1,40 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Veridise Saas Documentation',
-  tagline: 'Learn how to use Veridise tools',
-  favicon: 'img/favicon.ico',
+  title: "Veridise Saas Documentation",
+  tagline: "Learn how to use Veridise tools",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: "https://your-docusaurus-test-site.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-	  exclude: [
-	    "orca/*",
-	    "vanguard/*"
-	  ]
+          sidebarPath: require.resolve("./sidebars.js"),
+          exclude: ["orca/*", "vanguard/*"],
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl:
@@ -54,7 +51,7 @@ const config = {
         },
         */
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -62,134 +59,130 @@ const config = {
 
   plugins: [
     [
-      '@docusaurus/plugin-content-docs',
+      "@docusaurus/plugin-content-docs",
       {
-        id: 'saas',
-        path: 'saas',
-        routeBasePath: 'saas',
-        sidebarPath: require.resolve('./sidebars.js'),
-        lastVersion: 'current',
-      }
+        id: "saas",
+        path: "saas",
+        routeBasePath: "saas",
+        sidebarPath: require.resolve("./sidebars.js"),
+        lastVersion: "current",
+      },
     ],
     [
-      '@docusaurus/plugin-content-docs',
+      "@docusaurus/plugin-content-docs",
       {
-        id: 'zkvanguard',
-        path: 'zkvanguard',
-        routeBasePath: 'zkvanguard',
-        sidebarPath: require.resolve('./sidebars.js'),
-        lastVersion: 'current',
-        sidebarCollapsed: false
-      }
+        id: "zkvanguard",
+        path: "zkvanguard",
+        routeBasePath: "zkvanguard",
+        sidebarPath: require.resolve("./sidebars.js"),
+        lastVersion: "current",
+        sidebarCollapsed: false,
+      },
     ],
     [
-      '@docusaurus/plugin-content-docs',
+      "@docusaurus/plugin-content-docs",
       {
-        id: 'picus',
-        path: 'picus',
-        routeBasePath: 'picus',
-        sidebarPath: require.resolve('./sidebars.js'),
-        lastVersion: 'current',
-      }
+        id: "picus",
+        path: "picus",
+        routeBasePath: "picus",
+        sidebarPath: require.resolve("./sidebars.js"),
+        lastVersion: "current",
+      },
     ],
     [
-      require.resolve('docusaurus-lunr-search'),
+      require.resolve("docusaurus-lunr-search"),
       {
-	// Do not index pages with routes like /orca/internal/dev-guide etc.
-	excludeRoutes: [
-	  "**/internal/*",
-	  "orca/*",
-	  "vanguard/*"
-	]
-      }
-    ]
+        // Do not index pages with routes like /orca/internal/dev-guide etc.
+        excludeRoutes: ["**/internal/*", "orca/*", "vanguard/*"],
+      },
+    ],
   ],
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       colorMode: {
-	respectPrefersColorScheme: true
+        respectPrefersColorScheme: true,
       },
       // Replace with your project's social card
       // image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'Veridise SaaS Documentation',
+        title: "Veridise SaaS Documentation",
         logo: {
-          alt: 'Veridise Logo',
-          src: 'img/veridise-logo-round-blue.svg',
+          alt: "Veridise Logo",
+          src: "img/veridise-logo-round-blue.svg",
         },
         items: [
           {
-            type: 'docsVersion',
-            docsPluginId: 'saas',
-            position: 'left',
-            label: 'SaaS',
+            type: "docsVersion",
+            docsPluginId: "saas",
+            position: "left",
+            label: "SaaS",
           },
-	  {
-	    // TODO: replace with dropdown when versions are available.
-	    type: 'docsVersion',
-	    docsPluginId: 'zkvanguard',
-	    position: 'left',
-	    label: 'ZK Vanguard',
-	  },
-	  {
-	    // TODO: replace with dropdown when versions are available.
-	    type: 'docsVersion',
-	    docsPluginId: 'picus',
-	    position: 'left',
-	    label: 'Picus',
-	  },
+          {
+            // TODO: replace with dropdown when versions are available.
+            type: "docsVersion",
+            docsPluginId: "zkvanguard",
+            position: "left",
+            label: "ZK Vanguard",
+          },
+          {
+            // TODO: replace with dropdown when versions are available.
+            type: "docsVersion",
+            docsPluginId: "picus",
+            position: "left",
+            label: "Picus",
+          },
           // {to: '/blog', label: 'Blog', position: 'left'},
-	  {
-	    type: 'docsVersionDropdown',
-	    docsPluginId: 'orca',
-	    position: 'right'
-	  },
-	  {
-	    type: 'docsVersionDropdown',
-	    docsPluginId: 'vanguard',
-	    position: 'right'
-	  },
-	  {
-	    type: 'docsVersionDropdown',
-	    docsPluginId: 'zkvanguard',
-	    position: 'right'
-	  },
-	  {
-	    type: 'docsVersionDropdown',
-	    docsPluginId: 'picus',
-	    position: 'right'
-	  }
+          {
+            type: "docsVersionDropdown",
+            docsPluginId: "orca",
+            position: "right",
+          },
+          {
+            type: "docsVersionDropdown",
+            docsPluginId: "vanguard",
+            position: "right",
+          },
+          {
+            type: "docsVersionDropdown",
+            docsPluginId: "zkvanguard",
+            position: "right",
+          },
+          {
+            type: "docsVersionDropdown",
+            docsPluginId: "picus",
+            position: "right",
+          },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: "Tutorial",
+                to: "/docs/intro",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               // {
               //   label: 'Discord',
               //   href: 'https://discordapp.com/invite/yoururlhere',
               // },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/VeridiseInc',
+                label: "Twitter",
+                href: "https://twitter.com/VeridiseInc",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               /*
               {
@@ -198,12 +191,12 @@ const config = {
               },
               */
               {
-                label: 'Homepage',
-                href: 'https://veridise.com/',
+                label: "Homepage",
+                href: "https://veridise.com/",
               },
               {
-                label: 'Blog',
-                href: 'https://veridise.medium.com/',
+                label: "Blog",
+                href: "https://veridise.medium.com/",
               },
             ],
           },
@@ -213,7 +206,7 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
-        additionalLanguages: ['solidity']
+        additionalLanguages: ["solidity"],
       },
     }),
 };
