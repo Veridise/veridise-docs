2db47980b2187ecfc3a46d9deee6384d2e5c648b
Bryan Tan <bryan@veridise.com>
2024-10-08 11:15:03 -0700

    Update site title to use AuditHub instead of SaaS

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,12 +9,12 @@ const dashes = require("./src/remark/dashes");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Veridise Saas Documentation",
+  title: "AuditHub Documentation",
   tagline: "Learn how to use Veridise tools",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-test-site.com",
+  url: "https://docs.veridise.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
